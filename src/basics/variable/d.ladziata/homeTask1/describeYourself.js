eec7e56eff2e5e1557b044c24618b8f20028329b
Dzmitry LadziataE <ladyatadima@gmail.com>
2023-05-28 23:44:11 +0200

    Change main folder name, add two files with shop and myself descriptions

---
+++ src/basics/variable/d.ladziata/homeTask1/describeYourself.js
@@ -0,0 +1,16 @@
+let firstName;
+let sername;
+firstName = "Dzmitry";
+sername = "Ladziata";
+let age;
+console.log(`${firstName} ${sername}`);
+
+let param = {
+    height: "184 cm",
+    weight: "83 kg",
+    eyes: "brown",
+    age: 25,
+    hobbies: [
+        "tennis", "board games", "computer games"
+    ]
+};
---
+++ src/basics/variable/d.ladziata/homeTask1/shop.js
@@ -0,0 +1,13 @@
+let shop;
+shop = "Meat";
+console.log(`${shop}`);
+let storeDescription = {
+    goods: 10000,
+    employees: [ "Bob", "Rob", "Lilia" ],
+    products: {
+        chicken: 10,
+        pork: 15,
+        turkey: 20
+    },
+    open: true || false
+};
