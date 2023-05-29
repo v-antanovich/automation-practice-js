4a0891d021de0165f7688c7b9b5a522fc3c20f9a
HalinaS <h.savich@godeltech.com>
2023-05-29 11:55:16 +0200

    Add Task1 and Task2 for 'variables' topic

---
+++ community/h.savich/variables_hs.js
@@ -0,0 +1,29 @@
+//Task 1
+
+let period = "Summer";
+let shopName = `${period} Fashion`;
+
+console.log(shopName);
+
+let storeDescription = {
+    goods: 10000,
+    employees: ['Ivanov', 'Petrov', 'Sidorov'],
+    products: [{goods: "T-shirt", price:10}, {goods:"shoes", price:50}, {goods:"dress", price:10}],
+    open: true
+};
+console.log(storeDescription.goods)
+console.log(storeDescription.products[1].price)
+
+ //Task 2
+ let myName = 'Halina';
+ let age = 28;
+ let appearance = {
+    hairs: 'fair-haired',
+    eyes: 'blue'
+ };
+
+ let hobbies = ['walking', 'watching movies', 'coffee break'];
+
+
+ console.log(appearance.hairs);
+ console.log(myName.toUpperCase())
