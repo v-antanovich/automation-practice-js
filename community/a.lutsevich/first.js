e2a62ca2ef71e5ccbf170cc9fccf453db28039c9
VovaAltabel <vladimir.tereschenko@altabel.com>
2023-06-01 22:25:29 +0200

    added first task answer

---
+++ community/a.lutsevich/first.js
@@ -0,0 +1,11 @@
+// First task
+
+const shopNumber = 24;
+const shopName = `Shop ${shopNumber}`;
+
+const storeDescription = {
+  goods: 1000,
+  employees: ["Sasha", "Ira", "Sviatlana"],
+  products: { apple: 1, parrot: 2, potato: 3 },
+  open: true,
+};
