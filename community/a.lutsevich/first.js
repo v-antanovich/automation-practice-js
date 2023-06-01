0609e91e09d54c5099b3f4416fa179b42da0981b
VovaAltabel <vladimir.tereschenko@altabel.com>
2023-06-01 22:26:18 +0200

    added second task answer

--- community/a.lutsevich/first.js
+++ community/a.lutsevich/first.js
@@ -9,3 +9,20 @@ const storeDescription = {
   products: { apple: 1, parrot: 2, potato: 3 },
   open: true,
 };
+
+// Second task
+
+const KeyToMe = Symbol("food");
+const myPersonality = {
+  name: "Aliaksandr",
+  age: 26,
+  employee: true,
+  skills: ["JS", "React", "MaterialUI"],
+  flaws: undefined,
+  money: null,
+  dreams: 123n,
+  [KeyToMe]: "Draniki",
+  introduction() {
+    console.log(`Hello everyone, my name is ${this.name}`);
+  },
+};
