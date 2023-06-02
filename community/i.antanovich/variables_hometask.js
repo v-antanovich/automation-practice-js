5956af70b8a6e4c7e723370ae6f09d63f0510f87
iryna antanovich <irinantanovich@gmail.com>
2023-06-02 10:59:16 +0200

    Variables has been saved

---
+++ community/i.antanovich/variables_hometask.js
@@ -0,0 +1,29 @@
+let shop = "Beauty!";
+console.log(`Shop: ${shop}`);
+
+let storeDescription = {
+    goods: 10000,
+    employees: ["Bob", "Anna", "Tom"],
+    products: {
+        lotion: 40,
+        cream: 50,
+        soap: 20,
+        shampoo: 56
+    },
+isOpen: true
+};
+
+
+let user = {
+    firstName: "Iryna",
+    lastName: "Antanovich",
+    age: 33,
+    apperance: {
+        eyes: "hazel",
+        hair: "brown"
+    },
+
+    hobby: ["Reading", "Yoga", "Travelling", "Learning"],
+    isMarried: true
+    
+}
