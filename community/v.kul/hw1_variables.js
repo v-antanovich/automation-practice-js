fe2850bc4533458c2f28e4eca17f1414f212a066
ViktoriaKul <viktoriyakul7@gmail.com>
2023-06-02 09:59:32 +0200

    hw1 variables

---
+++ community/v.kul/hw1_variables.js
@@ -0,0 +1,19 @@
+//Task 1
+let storeType = 'Candy';
+let storeName = `${storeType} store`;
+console.log(`New store name is ${storeName}`);
+
+let storeDescription = { goods: 10000, employees: ["Sam", "Jim", "Pam"], products: { candies: 100, icecream: 150, cookies: 100 }, open: true };
+console.log(storeDescription);
+
+// Task 2
+let aboutMe = {};
+aboutMe.height = 162;
+aboutMe.age = 24;
+aboutMe.hobbies = ['sport', 'singing', 'psycology', 'reading', 'baking', 'travelling']
+aboutMe.relocated = true;
+aboutMe.pets = {dog:"ruby", cat:"cutie"}
+console.log(aboutMe)
+
+
+
