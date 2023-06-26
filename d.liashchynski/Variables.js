9e896b7239e4680aae1b1c3692403e633d3d19bc
Дмитрий Лещинский <dmitry.leshchinsky@gmail.com>
2023-06-26 23:59:38 +0200

    Added Variables task 1

---
+++ d.liashchynski/Variables.js
@@ -0,0 +1,21 @@
+// ------------------------ Task 1 ------------------------
+let myStore = 'MegaStore';
+let evaluate = 'Super';
+
+console.log(`${evaluate} ${myStore}`);
+
+let storeDescription = {
+    goods : 1000,
+    employees : ['Mark', 'Giles', 'John'],
+    products : {
+        meat : 100,
+        milk : 5,
+        bred : 1
+    },
+    open : true
+};
+
+console.log(storeDescription);
+
+
+// ------------------------ Task 2 ------------------------
