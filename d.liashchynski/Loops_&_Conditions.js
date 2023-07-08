8a55c28faef13be8f6747fa09d4e2c54684b369f
Дмитрий Лещинский <dmitry.leshchinsky@gmail.com>
2023-07-08 11:56:54 +0200

    Changed task1 to Switch, changed task7, removed code duplication

--- d.liashchynski/Loops_&_Conditions.js
+++ d.liashchynski/Loops_&_Conditions.js
@@ -4,7 +4,22 @@
 
 let login = 'admin';
 
-if (login == 'admin') {
+switch (login) {
+  case 'admin' :
+    console.log('Ты можешь все');
+    break;
+  case 'user' :
+    console.log('Привет юзер');
+    break;
+  case null :
+    console.log('Пока');
+    break;
+  default : 
+    console.log('Я тебя не знаю');
+    break;
+};
+
+/* if (login == 'admin') {
     console.log('Ты можешь все') 
 } else if (login == 'user') {
     console.log('Привет юзер')
@@ -12,7 +27,9 @@ if (login == 'admin') {
     console.log('Пока')
 } else {
     console.log('Я тебя не знаю')
-};
+}; */
+
+
 
 
 //2. Заполните массив из 10-и элементов случайными числами от 0 до 10.
@@ -39,14 +56,6 @@ console.log(copyRandomArr);
 
 
 // 4. Найдите минимальное и максимальные числа в массиве из второго задания.
-let arr = [];
-
-for (let i = 0; i < 10; i++) {
-  let randomArr = Math.floor(Math.random() * 11);
-  arr.push(randomArr);
-}
-console.log(arr);
-
 let minNumber = Math.min(...arr);
 let maxNumber = Math.max(...arr);
 
@@ -55,14 +64,6 @@ console.log('Max number: ' + maxNumber);
 
  
 // 5. Посчитайте количество чисел больше 5 в массиве из второго задания.
-let arr = [];
-
-for (let i = 0; i < 10; i++) {
-  let randomArr = Math.floor(Math.random() * 11);
-  arr.push(randomArr);
-}
-console.log(arr);
-
 let count = 0;
 
 for (let i = 0; i < arr.length; i++) {
@@ -76,16 +77,6 @@ console.log('Amount of numbers greater than 5: ' + count);
  
 /* 6. Сделайте копию массива из второго задания, развернув все его значения наоборот.
  Пример: Дан массив [0, 1, 2, 3, 4, 5]. Ответ должен быть [5, 4, 3, 2, 1, 0] */
-
-let arr = [];
-
-for (let i = 0; i < 10; i++) {
-  let randomArr = Math.floor(Math.random() * 11);
-  arr.push(randomArr);
-}
-
-console.log(arr);
-
 let copyArr = arr.slice().reverse();
 console.log('Reversed array: ' + copyArr);
 
@@ -101,9 +92,12 @@ let result, x=4, y=3;
 };
 console.log(result); */
 
-result = (x + y < 7) ? 'less' :
-result = (x + y > 7) ? result = 'more' : 'equel';
+result = (x + y < 7) ? 'less' : (x + y > 7) ? 'more': 'equel';
 console.log(result);
+
+/* result = (x + y < 7) ? 'less' :
+result = (x + y > 7) ? result = 'more' : 'equel';
+console.log(result); */
 
 
  
